0e95201869e3dccff08420edc5021ff1b0113ef9
designaval <1designaval@gmail.com>
2022-03-19 04:17:20 -0700

    ch

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+// --------------------- location --------------------
+function getLocation() {
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition);
+  }
+  else {
+      alert("Geolocation is not supported by your browser.");
+  }
+}
+
+function showPosition(position) {
+  alert("Latitude: " + position.coords.latitude + ", Longitude: " + position.coords.longitude);
+}
+
+getLocation();
+
+// -------------modal -----------------
 // Get the modal
 var modal = document.getElementById("myModal");
 
@@ -7,15 +24,6 @@ var btn = document.getElementById("myBtn");
 // Get the <span> element that closes the modal
 var span = document.getElementsByClassName("close")[0];
 
-// When the user clicks on the button, open the modal
-btn.onclick = function() {
-  modal.style.display = "block";
-}
-
-// When the user clicks on <span> (x), close the modal
-span.onclick = function() {
-  modal.style.display = "none";
-}
 
 // When the user clicks anywhere outside of the modal, close it
 window.onclick = function(event) {
